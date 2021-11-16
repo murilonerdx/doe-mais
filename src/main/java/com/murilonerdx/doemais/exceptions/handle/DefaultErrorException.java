558a57d9262dcc33bb9b0a5964267e117014280e
murilonerdx <mu-silva@outlook.com>
2021-11-16 18:40:37 -0300

    Configurando rota error

--- src/main/java/com/murilonerdx/doemais/exceptions/handle/DefaultErrorException.java
+++ src/main/java/com/murilonerdx/doemais/exceptions/handle/DefaultErrorException.java
@@ -1,11 +1,12 @@
 package com.murilonerdx.doemais.exceptions.handle;
 
+import com.murilonerdx.doemais.exceptions.DataIntegretyException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-class GlobalDefaultExceptionHandler {
-    @ExceptionHandler(value = Exception.class)
+class DefaultErrrorException {
+    @ExceptionHandler(value = DataIntegretyException.class)
     public String
     defaultErrorHandler() {
         return "error";
